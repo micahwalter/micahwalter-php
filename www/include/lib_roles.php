09c45467c4ca96bccbc98d45dff64f599e85b321
Micah Walter <micah@micahwalter.com>
2013-12-23 14:59:48 -0500

    stubbin out code

--- www/include/lib_roles.php
+++ www/include/lib_roles.php
@@ -3,18 +3,6 @@
 	#################################################################
 
 	function roles_get_by_id($id){
-
-		$user = users_get_by_id($id);
-	
-		$profile = array(
-			'user' => $user
-		);		
-		
-		$rsp = db_single(db_fetch("SELECT * FROM users_profile WHERE user_id=".intval($user['id'])));
-
-		$profile['profile'] = $rsp;
-	
-		return $profile;
 	
 	}
 	
@@ -33,21 +21,7 @@
 	#################################################################
 
 	function roles_update_role(&$user, $update){
-	
-		$hash = array();
-		foreach ($update as $k => $v){
-			$hash[$k] = AddSlashes($v);
-		}
 
-		$ret = db_update('users_profile', $hash, "user_id={$user['id']}");
-
-		if (!$ret['ok']) return $ret;
-
-		cache_unset("USER-{$user['id']}");
-
-		return array(
-			'ok' => 1,
-		);
 	
 	}
 
@@ -69,3 +43,12 @@
 		);
 	
 	}
+	
+	#################################################################
+
+	function roles_delete_role($id){
+
+	
+	}
+
+	
